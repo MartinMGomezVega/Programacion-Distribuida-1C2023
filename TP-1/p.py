cb215dcde3e414b5a5a2a3912e3e735f77f6897d
Randy Mendoza <randysley@gmail.com>
2023-05-08 20:11:30 -0300

    agregado de archivo inicial rcp.py y p.py

---
+++ TP-1/p.py
@@ -0,0 +1,18 @@
+import time
+
+def sumar(x, y):
+	return x+y
+
+#RPC
+def multiplicar(x,y):
+	return x*y
+
+x = 2
+y = 3
+
+print("Suma de los numeros:",sumar(x, y))
+print("multiplicacion de los numeros: ",multiplicar(x, y))
+
+
+	
+
