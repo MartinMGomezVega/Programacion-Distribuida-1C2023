771966eacb856a715027459b5e0de8ed7e6df720
MartinMGomezVega <martin_gomezvega@hotmail.com>
2023-05-17 22:03:57 -0300

    actualizado

--- TP-1/p.py
+++ TP-1/p.py
@@ -10,5 +10,10 @@ def multiplicar(x,y):
 x = 2
 y = 3
 
-print("Suma de los numeros: ", sumar(x, y))
-print("Multiplicacion de los numeros: ", multiplicar(x, y))
+# print("Suma de los numeros: ", sumar(x, y))
+# print("Multiplicacion de los numeros: ", multiplicar(x, y))
+
+print(sumar(x,y))
+for i in range(100):
+	time.sleep(1)
+	print("Multiplicacion de los numeros: ",multiplicar(i,i))
